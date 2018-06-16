03635fbbd2fbc05328ff4153cde39afe4e23218c
Ritwik36 <ishuritwik@gmail.com>
2018-06-16 12:50:21 +0530

    hi ritwik

--- Lab225/src/Lab225.java
+++ Lab225/src/Lab225.java
@@ -1,24 +1,24 @@
-class Lab225 {
-	public static void main(String args[]) {
-		Customer c1 = new Customer();
-		c1.show();
-		Customer c2 = new Customer();
-		c2.cid = 99;
-		c2.cname = "Sri";
-		c2.phone = 65799999;
-		c2.show();
-	}
+class Lab225{
+public static void main(String args[]){
+Customer c1 = new Customer();
+c1.show();
+Customer c2 = new Customer();
+c2.cid = 99;
+c2.cname = "Sri";
+c2.phone = 65799999;
+c2.show();
+}
 }
 
-class Customer {
-	int cid;
-	String cname;
-	long phone;
-
-	void show() {
-		System.out.println(cid);
-		System.out.println(cname);
-		System.out.println(phone);
-		System.out.println(phone);
-	}
+class Customer{
+int cid;
+String cname;
+long phone;
+void show(){
+System.out.println(cid);
+System.out.println(cname);
+System.out.println(phone);
+System.out.println(phone);
+System.out.println(ritwik);
+}
 }
